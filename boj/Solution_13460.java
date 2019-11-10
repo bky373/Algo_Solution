0365bec642bab67119756fa1ecd20644acb1fb9e
bky373 <bo373@naver.com>
2019-11-10 22:26:08 +0900

    Upload Solution_13460.java

---
+++ boj/Solution_13460.java
@@ -0,0 +1,17 @@
+package study.boj;
+
+import java.util.Scanner;
+
+public class Solution_13460 {
+	public static void main(String[] args) {
+		// 입력받기
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int cnt = 0;// 시도횟수
+		// 4개 동작 (왼쪽으로 기울이기, 오른쪽으로 기울이기, 위쪽으로 기울이기, 아래쪽으로 기울이기)
+		
+	}
+
+}
---
+++ dataStructure/Dfs_Test.java
@@ -0,0 +1,10 @@
+package study.dataStructure;
+
+public class Dfs_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
