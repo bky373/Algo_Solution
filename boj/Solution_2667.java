5d489bb708ee7e1a4a421c0ca377c49c88dd3b33
bky373 <bo373@naver.com>
2019-11-13 22:40:14 +0900

    Upload Solution_2667.java

---
+++ boj/Solution_2667.java
@@ -0,0 +1,14 @@
+package study.boj;
+
+import java.util.Scanner;
+
+public class Solution_2667 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[][] map = new int[N][N];
+		int cntComplex, cntEachComplex;
+		
+	}
+}
