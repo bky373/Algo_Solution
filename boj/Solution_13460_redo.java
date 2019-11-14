b9c719d94142d2dc03520bdc9042ec8abc2daf23
bky373 <bo373@naver.com>
2019-11-14 23:33:16 +0900

    redo boj_13460

--- boj/Solution_13460_redo.java
+++ boj/Solution_13460_redo.java
@@ -6,14 +6,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Node {
+class QNode {
 	int rRow;
 	int rCol;
 	int bRow;
 	int bCol;
 	int cnt;
 
-	public Node(int rRow, int rCol, int bRow, int bCol, int cnt) {
+	public QNode(int rRow, int rCol, int bRow, int bCol, int cnt) {
 		super();
 		this.rRow = rRow;
 		this.rCol = rCol;
@@ -22,14 +22,13 @@ class Node {
 		this.cnt = cnt;
 	}
 
-	public Node() {
+	public QNode() {
 	}
-
 }
 
-public class Solution_13460 {
-	static final int R = 0, B = 1;
+public class Solution_13460_redo {
 	static int N, M;
+	static int MAX = 10;
 	static char[][] map;
 	static boolean[][][][] visited;
 	static int[] dx = new int[] { 0, 0, 1, -1 }; // 동서 남북
@@ -40,11 +39,11 @@ public class Solution_13460 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-
+        
 		map = new char[N][M];
-		visited = new boolean[10][10][10][10];
+		visited = new boolean[MAX][MAX][MAX][MAX];
 
-		Node node = new Node();
+		QNode node = new QNode();
 		node.cnt = 0;
 
 		for (int i = 0; i < N; i++) {
@@ -63,17 +62,16 @@ public class Solution_13460 {
 		bfs(node);
 	}
 
-	private static void bfs(Node ball) {
+	private static void bfs(QNode in) {
 
-		Queue<Node> q = new LinkedList<Node>();
-		q.offer(ball);
+		Queue<QNode> q = new LinkedList<QNode>();
+		q.offer(in);
 
 		while (!q.isEmpty()) {
-
-			Node node = q.poll();
+			QNode node = q.poll();
 			visited[node.rRow][node.rCol][node.bRow][node.bCol] = true;
 
-			// 11번 이상 굴렸을 경우 -1을 출력한다.
+			// 11번 이상이면 -1 리턴
 			if (node.cnt >= 10) {
 				System.out.println(-1);
 				return;
@@ -82,7 +80,7 @@ public class Solution_13460 {
 			// 현재 두 구슬의 위치를 기준으로 동서남북으로 굴려본다.
 			for (int i = 0; i < 4; i++) {
 
-				// 파란색 구슬을 먼저 굴린다.
+				// 파란 구슬 먼저 굴린다.
 				int b_nRow = node.bRow;
 				int b_nCol = node.bCol;
 				while (map[b_nRow + dx[i]][b_nCol + dy[i]] != '#') {
@@ -93,7 +91,7 @@ public class Solution_13460 {
 					}
 				}
 
-				// 그 다음, 빨간색 구슬을 굴린다.
+				// 그 다음, 빨간 구슬을 굴린다.
 				int r_nRow = node.rRow;
 				int r_nCol = node.rCol;
 				while (map[r_nRow + dx[i]][r_nCol + dy[i]] != '#') {
@@ -104,12 +102,12 @@ public class Solution_13460 {
 					}
 				}
 
-				// 파란색 구슬이 'O'에 빠졌다면, 탐색을 멈춘다.
+				// 파란 구슬이 'O'에 빠졌다면, 탐색을 멈춘다.
 				if (map[b_nRow][b_nCol] == 'O') {
 					continue;
 				}
-
-				// 빨간색 구슬만 'O'에 빠졌다면, 정답을 출력한다.
+				
+				// 빨간 구슬이 'O'에 빠졌다면, 정답을 출력한다.
 				if (map[r_nRow][r_nCol] == 'O') {
 					System.out.println(node.cnt + 1);
 					return;
@@ -117,6 +115,7 @@ public class Solution_13460 {
 
 				// 두 구슬의 위치가 같다면, 위치를 조정한다.
 				if (r_nRow == b_nRow && r_nCol == b_nCol) {
+
 					switch (i) {
 
 					case 0: // 동
@@ -132,14 +131,14 @@ public class Solution_13460 {
 						else
 							b_nCol += 1;
 						break;
-						
+
 					case 2: // 남
 						if (node.rRow > node.bRow)
 							b_nRow -= 1;
 						else
 							r_nRow -= 1;
 						break;
-
+						
 					case 3: // 북
 						if (node.rRow > node.bRow)
 							r_nRow += 1;
@@ -148,21 +147,11 @@ public class Solution_13460 {
 						break;
 					}
 				}
-				//두 구슬을 구린 후의 각각의 위치가 처음 탐색하는 것이라면 큐에 넣는다.
+				
 				if(!visited[r_nRow][r_nCol][b_nRow][b_nCol])
-					q.offer(new Node(r_nRow, r_nCol, b_nRow, b_nCol, node.cnt + 1));
+					q.offer(new QNode(r_nRow, r_nCol, b_nRow, b_nCol, node.cnt+1));
 			}
 		}
 		System.out.println(-1);
 	}
-	
-	public static void showMap(char[][] map) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
