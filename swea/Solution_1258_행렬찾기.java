c82b22c246eb7b9ec182ad7e2e01f1514d8a2f25
bky373 <bo373@naver.com>
2019-11-18 21:56:51 +0900

    made function(sortRC) in Solution_1258

--- swea/Solution_1258_행렬찾기.java
+++ swea/Solution_1258_행렬찾기.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public class Solution_1258_행렬찾기 {
 	static int T, n, k, cnt_subMatrix;
-	static int[][] map, tmpMap;
-	static int[] r, c;
+	static int[][] map, arrRC;
+	static int[] rowArr, colArr;
 	static int[] dx = { 0, 1 }; // 동 남
 	static int[] dy = { 1, 0 };
+	static HashMap<Integer, Integer> hm;
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -21,9 +22,10 @@ public class Solution_1258_행렬찾기 {
 				map[i][j] = sc.nextInt();
 			}
 		}
-		
-		r = new int[20];
-		c = new int[20];
+		arrRC = new int[20][3];
+		rowArr = new int[20];
+		colArr = new int[20];
+		cnt_subMatrix = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (map[i][j] != 0) {
@@ -38,61 +40,57 @@ public class Solution_1258_행렬찾기 {
 				}
 			}
 		}
-		Object[] list = sortRC(r, c);
-		System.out.println(Arrays.toString(r));
-		System.out.println(Arrays.toString(c));
-		System.out.println(Arrays.toString(list));
+		sortRC(arrRC);
+		for (int[] i : arrRC) {
+			if (i[0] != 0)
+				System.out.print(i[0] + " " + i[1] + " ");
+		}
+//		System.out.println(Arrays.toString(arrRC[0]));
+//		System.out.println(Arrays.toString(arrRC[1]));
+//		System.out.println(Arrays.toString(arrRC[2]));
+//		System.out.println(Arrays.toString(list));
 		System.out.println(cnt_subMatrix);
-		cnt_subMatrix = 0;
 	}
 
 	private static boolean isException(int cnt_subMatrix) {
 		if (80 < n && n <= 100)
-			return (15 < cnt_subMatrix && cnt_subMatrix <= 20)? false : true;
+			return (15 < cnt_subMatrix && cnt_subMatrix <= 20) ? false : true;
 		else if (n > 40)
-			return (10 < cnt_subMatrix && cnt_subMatrix <= 15)? false : true;
+			return (10 < cnt_subMatrix && cnt_subMatrix <= 15) ? false : true;
 		else if (n > 10)
-			return (5 < cnt_subMatrix && cnt_subMatrix <= 10)? false : true;
-		else return false;
+			return (5 < cnt_subMatrix && cnt_subMatrix <= 10) ? false : true;
+		else
+			return false;
 	}
 
 	private static void cntSubRCs(int[][] map, int i, int j) {
 		int x = i, y = j;
 		if (k < 20) {
 			while (x < n && map[x][y] != 0) {
-				r[k] += 1;
+				arrRC[k][0] += 1;
+				rowArr[k] += 1;
 				x += 1;
 			}
 			x = i;
 			while (y < n && map[x][y] != 0) {
-				c[k] += 1;
+				arrRC[k][1] += 1;
+				colArr[k] += 1;
 				y += 1;
 			}
 			k++;
+			for (int n = 0; n < k; n++)
+				arrRC[n][2] = arrRC[n][0] * arrRC[n][1];
 		}
 	}
 
-	private static Object[] sortRC(int[] rowArr, int[] colArr) {
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-		for (int i = 0; i < rowArr.length; i++)
-			if (rowArr[i] != 0)
-				hm.put(rowArr[i], rowArr[i] * colArr[i]);
-
-		List<Integer> list = new ArrayList<Integer>();
-		list.addAll(hm.keySet());
-
-		Collections.sort(list, new Comparator<Object>() {
-
-			public int compare(Object o1, Object o2) {
-				int v1 = hm.get(o1);
-				int v2 = hm.get(o2);
-
-				return ((Comparable) v2).compareTo(v1);
+	private static void sortRC(int[][] arrRC) {
+		Arrays.sort(arrRC, new Comparator<int[]>() {
+			public int compare(int[] entry1, int[] entry2) {
+				int v1 = entry1[2];
+				int v2 = entry2[2];
+				return v1 != v2 ? Integer.compare(v1, v2) : entry1[0] - entry2[1];
 			}
 		});
-
-		Collections.reverse(list);
-		return list.toArray();
 	}
 
 	private static void Search(Dot d1) {
