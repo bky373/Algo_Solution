5780a1dac62ca46a39e3fadf678a1c5b720ba3d6
bky373 <bo373@naver.com>
2019-11-21 08:40:40 +0900

    solved 1223_계산기2 in another way

---
+++ swea/Solution_1223_계산기2_2.java
@@ -0,0 +1,90 @@
+package study.swea;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution_1223_°è»ê±â2_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=1; i<11; i++) {
+			int size = sc.nextInt();
+			String infix = sc.next();
+			
+			String postfix = toPostfix(infix);
+			System.out.printf("#%d %d\n", i, (int)calc(postfix));
+		}
+	}
+	
+	private static String toPostfix(String infix) {
+		StringBuilder postfix = new StringBuilder();
+		char[] arrChar = infix.toCharArray();
+		Stack<Character> stack = new Stack<Character>();
+		
+		for(int i=0;i<arrChar.length;i++) {
+			char ch = arrChar[i];
+			if(Character.isDigit(ch))
+					postfix.append(ch);
+			else if(ch=='+' ||ch=='-' ||ch=='*' ||ch=='/') {
+				int rank = toRank(ch);
+				while(!stack.isEmpty()) {
+					if( toRank(stack.peek()) < rank) break;
+					postfix.append(stack.pop());
+				}
+				stack.push(ch);
+			}
+		}
+		
+		while(!stack.isEmpty()) 
+			postfix.append(stack.pop());
+		
+		return postfix.toString();
+	}
+	
+	private static int toRank(char ch) {
+		if(ch=='*' ||ch=='/') return 2;
+		else if (ch=='+' ||ch=='-') return 1;
+		return 0;
+	}
+	
+	private static int calc(String postifx) {
+		char[] charArr = postifx.toCharArray();
+		Stack<Integer> stack = new Stack<Integer>();
+		int v1, v2;
+		
+		for(int i=0;i<charArr.length;i++) {
+			char ch = charArr[i];
+			if(Character.isDigit(ch))
+				stack.push(ch -'0');
+
+			switch(ch) {
+			
+			case '+' : 
+				v1 = stack.pop();
+				v2 = stack.pop();
+				stack.push(v2 + v1);
+				break;
+				
+			case '-' : 
+				v1 = stack.pop();
+				v2 = stack.pop();
+				stack.push(v2 - v1);
+				break;
+				
+			case '*' : 
+				v1 = stack.pop();
+				v2 = stack.pop();
+				stack.push(v2 * v1);
+				break;
+				
+			case '/' : 
+				v1 = stack.pop();
+				v2 = stack.pop();
+				stack.push(v2 / v1);
+				break;
+			}
+		}
+		return stack.pop();
+	}
+}
