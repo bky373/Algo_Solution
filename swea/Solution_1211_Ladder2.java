fe7731cb00a7c06b1b68957a3f0691cbea5785a9
bky373 <bo373@naver.com>
2019-11-26 23:19:01 +0900

    solved 1211_Ladder2

--- swea/Solution_1211_Ladder2.java
+++ swea/Solution_1211_Ladder2.java
@@ -1,15 +1,13 @@
 package study.swea;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Solution_1210_Ladder1 {
+public class Solution_1211_Ladder2 {
 	static int n;
 	static int SIZE = 100;
 	static int[][] map;
-	static int[] arrCnt;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -24,57 +22,50 @@ public class Solution_1210_Ladder1 {
 				}
 			}
 
-			arrCnt = new int[100];
-			int min = Integer.MAX_VALUE, j = 0;
-			
+			int endY = 0;
 			for (int i = 0; i < SIZE; i++) {
-				if (map[0][i] == 1) {
-					arrCnt[i] = Search(new Location1(0, i));
-					if (arrCnt[i] <= min) {
-						min = arrCnt[i];
-						j = i;
-					}
+				if (map[SIZE - 1][i] == 2) {
+					endY = i;
+					break;
 				}
 			}
 			
-			System.out.printf("#%d %d\n", n, j);
+			int ans = Search(new Location(SIZE - 1, endY));
+			
+			System.out.printf("#%d %d\n", n, ans);
 		}
 	}
 
-	private static int Search(Location1 start) {
-		int nx = start.x;
-		int ny = start.y;
-		int cnt = 0;
+	private static int Search(Location last) {
+		int nx = last.x;
+		int ny = last.y;
 
-		while (nx != SIZE - 1) {
-			nx = nx + 1;
-			cnt++;
-
+		while (nx != 0) {
+			nx = nx - 1; // 한 칸씩 위로 이동
+			
 			// 왼쪽으로 길이 나 있을 때 왼쪽 끝까지 이동
 			if (ny - 1 >= 0 && map[nx][ny - 1] == 1) {
 				while (ny - 1 >= 0 && map[nx][ny - 1] != 0) {
 					ny = ny - 1;
-					cnt++;
 				}
-			}
-
+			} 
+			
 			// 오른쪽으로 길이 나 있을 때 오른쪽 끝까지 이동
 			else if (ny + 1 <= SIZE - 1 && map[nx][ny + 1] == 1) {
 				while (ny + 1 <= SIZE - 1 && map[nx][ny + 1] != 0) {
 					ny = ny + 1;
-					cnt++;
 				}
 			}
 		}
-		return cnt;
+		return ny;
 	}
 }
 
-class Location1 {
+class Location {
 	int x;
 	int y;
 
-	Location1(int x, int y) {
+	Location(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
